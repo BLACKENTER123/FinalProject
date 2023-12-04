6c5662150c7b0c6383cabd316dd9a71bce086ba7
Bryan <bry.chavez@duocuc.cl>
2023-12-04 17:15:34 -0300

    Listo Proyecto

--- restaurant/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Bookings, Menu, OrderItem, Orders, Cart
-# Register your models here.
-admin.site.register(Bookings)
-admin.site.register(Menu)
-admin.site.register(OrderItem)
-admin.site.register(Orders)
-admin.site.register(Cart)
